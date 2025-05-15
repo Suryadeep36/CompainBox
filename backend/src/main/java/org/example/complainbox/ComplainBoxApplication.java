546a3e343d5381d2618bd53112fbcf0673713043
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2025-05-15 12:01:39 +0530

    init the project

---
+++ backend/src/main/java/org/example/complainbox/ComplainBoxApplication.java
@@ -0,0 +1,11 @@
+package org.example.complainbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplainBoxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComplainBoxApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/org/example/complainbox/ComplainBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.complainbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplainBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
